2fa5ae76052f16975bdc1427d0a67305beeb0815
uen0_ <sarasa4869@zoho.com>
2022-08-02 10:08:33 +0900

    [Update] 名称変更：cell→piece

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour
+public class Piece : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class Cell : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static implicit operator Piece(GameObject v)
+    {
+        throw new NotImplementedException();
     }
 }
