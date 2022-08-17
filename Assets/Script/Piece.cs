a81c2da83d6314ae36ca49b7287332767cf97d04
uen0_ <sarasa4869@zoho.com>
2022-08-17 23:58:44 +0900

    [Update] enumで黒白の変更

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -3,6 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+  public enum color
+{
+    black = 1,
+    white = -1
+}
+
 public class Piece : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -21,4 +27,6 @@ public class Piece : MonoBehaviour
     {
         throw new NotImplementedException();
     }
+
+
 }
